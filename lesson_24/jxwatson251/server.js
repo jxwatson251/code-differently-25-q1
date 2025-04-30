26bce5008e62c8724909fcbd1a50ffb7bb317b57
jxwatson251 <jxwatson251@gmail.com>
2025-04-30 16:12:25 +0000

    lesson 24

--- lesson_24/jxwatson251/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/', (req, res) => {
-    console.log("Here")
-    res.sendStatus(500).json({ message: "Error" })
-})
-
-app.listen(3000)
