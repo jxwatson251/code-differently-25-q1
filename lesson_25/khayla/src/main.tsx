dd5e6ac088445fb577162184969d6eef896c7944
Khayla Saunders <kksaunders251@gmail.com>
2025-05-21 16:01:45 -0400

    feat: add khayla program components, second page, and routes in typescript (#603)
    * feat: adds unique folder
    
    * Feat: updates second page and routes
    
    * Feat: fixes file path, updates naming conventions, and installs node modules in correct space
    
    * Feat: updates imports for program
    
    * feat: add AddProgram page with form and route setup
    
    Includes routing via React Router and basic form structure for adding new programs.
    
    * chore: merge two khayla folders into one + add styling to second page
    
    ---------
    
    Co-authored-by: Vicente Vigueras <vicente.viguerassilva@gmail.com>

---
+++ lesson_25/khayla/src/main.tsx
@@ -0,0 +1,36 @@
+import App from './App.tsx';
+import {Home} from './pages/Home/Home.tsx';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+import './index.scss';
+import { SecondPage } from './pages/SecondPage/SecondPage.tsx';
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/second-page',
+        element: <SecondPage />,
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
